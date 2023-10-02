3a25bc71b59622bb16147b132daa84578f266bed
Sonya1488 <127775334+Sofiia-28@users.noreply.github.com>
2023-10-02 23:19:53 +0300

    edit1

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,66 +1,40 @@
-import axios from "axios";
-import Notiflix from 'notiflix';
-import {catCard, breedSelect, createCatCardMarkup, createSelectMarkup} from './index.js'
-import SlimSelect from 'slim-select'
+import axios from 'axios';
 
-// new SlimSelect({
-//   select: breedSelect,
-// })
+import {
+  catCard,
+  breedSelect,
+  loader,
+} from './index.js';
 
-const API_KEY = "live_Eo9WWuFEbqhH5kqqFsiPK3PaoOXAmftsLGMmpkhH78a4WfAsOyT5TpfWOn12afqP";
+const API_KEY =
+  'live_Eo9WWuFEbqhH5kqqFsiPK3PaoOXAmftsLGMmpkhH78a4WfAsOyT5TpfWOn12afqP';
 const BASE_URL = 'https://api.thecatapi.com/v1';
-axios.defaults.headers.common["x-api-key"] = API_KEY;
-const loader = document.querySelector(".loader");
-const error = document.querySelector(".error");
-error.style.display = "none";
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
 
 export function fetchBreeds() {
-    const END_POINT = '/breeds';
-    breedSelect.style.display = "none";
-  
-    return fetch(`${BASE_URL}${END_POINT}`)
-    .then(resp => {
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-      return resp.json();
-    })
-    .then((data) => {
-      breedSelect.insertAdjacentHTML("afterBegin", createSelectMarkup(data));
-      breedSelect.style.display = "block";
-        loader.style.display = "none";
-        return 
-    })
-    .catch((err) => {
-      console.log(err);
-      loader.style.display = "none";
-      Notiflix.Notify.failure(error.textContent);
-    })
-  }
+  const END_POINT = '/breeds';
+  breedSelect.style.display = 'none';
 
-  export function fetchCatByBreed(breedId) {
-    const END_POINT = `/images/search?breed_ids=${breedId}`;
-    loader.style.display = "block";
-    catCard.style.display = "none";
-  
-    return fetch(`${BASE_URL}${END_POINT}&api_key=${API_KEY}`)
+  return fetch(`${BASE_URL}${END_POINT}`)
     .then(resp => {
       if (!resp.ok) {
         throw new Error(resp.statusText);
       }
       return resp.json();
     })
-    .then((data) => {
-        catCard.insertAdjacentHTML("afterBegin", createCatCardMarkup(data));
-        catCard.style.display = "flex";
-        loader.style.display = "none";
-        return
+}
+
+export function fetchCatByBreed(breedId) {
+  const END_POINT = `/images/search?breed_ids=${breedId}`;
+  loader.style.display = 'block';
+  catCard.style.display = 'none';
+
+  return fetch(`${BASE_URL}${END_POINT}&api_key=${API_KEY}`)
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
     })
-    .catch((err) => {
-      console.log(err);
-      loader.style.display = "none";
-      Notiflix.Notify.failure(error.textContent);
-    })
-  }
-  
-  
+}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
-import {fetchBreeds, fetchCatByBreed} from './cat-api.js'
-import SlimSelect from 'slim-select'
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 
-export const breedSelect = document.querySelector(".breed-select") 
-export const catCard = document.querySelector(".cat-info")
+export const breedSelect = document.querySelector('.breed-select');
+export const catCard = document.querySelector('.cat-info');
+export const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+error.style.display = 'none';
 
-// new SlimSelect({
-//   select: breedSelect,
-// })
+fetchBreeds()
+  .then(data => {
+    breedSelect.insertAdjacentHTML('afterBegin', createSelectMarkup(data));
+    breedSelect.style.display = 'block';
+    loader.style.display = 'none';
+    return;
+  })
+  .catch(err => {
+    console.log(err);
+    loader.style.display = 'none';
+    Notiflix.Notify.failure(error.textContent);
+  });
 
-fetchBreeds();
-breedSelect.addEventListener("change", chooseCat);
+breedSelect.addEventListener('change', chooseCat);
 
 function chooseCat(event) {
-    event.preventDefault();
-    const breedId = breedSelect.value;
-    fetchCatByBreed(breedId);
+  event.preventDefault();
+  const breedId = breedSelect.value;
+  fetchCatByBreed(breedId)
+    .then(data => {
+      catCard.innerHTML = '';
+      catCard.insertAdjacentHTML('afterBegin', createCatCardMarkup(data));
+      catCard.style.display = 'flex';
+      loader.style.display = 'none';
+      return;
+    })
+    .catch(err => {
+      console.log(err);
+      loader.style.display = 'none';
+      Notiflix.Notify.failure(error.textContent);
+    });
 }
 
 export function createCatCardMarkup(arr) {
-    return arr.map(({url, breeds: [{name, description, temperament}]}) =>
-    `<img class="cat-image" src="${url}" alt="${name} cat" width="400"/>
+  return arr
+    .map(
+      ({ url, breeds: [{ name, description, temperament }] }) =>
+        `<img class="cat-image" src="${url}" alt="${name} cat" width="400"/>
     <div class="cat-description"> <h1 class="cat-name">${name}</h1>
     <p>${description}</p>
     <p><b>Temperament: </b>${temperament}</p></div>`
-    ).join("")
+    )
+    .join('');
 }
 
 export function createSelectMarkup(arr) {
-    return arr.map(({name, id}) => `<option value="${id}">${name}</option>`).join("")
+  return arr
+    .map(({ name, id }) => `<option value="${id}">${name}</option>`)
+    .join('');
 }
-
